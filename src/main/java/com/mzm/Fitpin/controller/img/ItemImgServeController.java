89ea37aa597d6bb4c050c2b037856586b90f913d
S-W-J <devilonprison@gmail.com>
2024-09-08 00:36:32 +0900

    이미지서브 코드 (엔드포인트)변경

--- src/main/java/com/mzm/Fitpin/controller/img/ItemImgServeController.java
+++ src/main/java/com/mzm/Fitpin/controller/img/ItemImgServeController.java
@@ -4,8 +4,8 @@ import com.mzm.Fitpin.service.img.ImageServeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +15,12 @@ public class ItemImgServeController {
     @Autowired
     private ImageServeService imageServeService;
 
-    @GetMapping
-    public ResponseEntity<?> getImage(@RequestParam("imgUrl") String imgUrl) {
+    @GetMapping("/{imageName}")
+    public ResponseEntity<?> getImage(@PathVariable String imageName) {
+        // 기본 경로 설정 (이미지 파일이 있는 디렉토리)
         String basePath = "/home/ubuntu/home/fitpin_backend/home/itemImg";
+        // 파일 전체 경로를 전달
+        String imgUrl = basePath + "/" + imageName;
         return imageServeService.serveImage(imgUrl, basePath);
     }
 }
