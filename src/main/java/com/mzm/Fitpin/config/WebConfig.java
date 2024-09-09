384570e5201234dfa2d842e3b2c22797a2139d6c
S-W-J <devilonprison@gmail.com>
2024-09-10 04:10:38 +0900

    CORS 설정 수정

--- src/main/java/com/mzm/Fitpin/config/WebConfig.java
+++ src/main/java/com/mzm/Fitpin/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.mzm.Fitpin.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // "/api/"로 시작하는 모든 경로에 대해 CORS 허용
-                .allowedOrigins("*")  // 모든 도메인 허용
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:8080", "http://fitpitback.kro.kr:8080")  // 로컬 및 배포 도메인 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true); // 자격 증명 허용 (쿠키, Authorization 헤더)
