96f54346a45f32cc9f33fc30596a41732d1423f2
S-W-J <devilonprison@gmail.com>
2024-09-20 13:36:49 +0900

    검색 API에 이미지 저장경로도 같이 리턴하도록..

---
+++ src/main/java/com/mzm/Fitpin/entity/ItemSearchwithImg.java
@@ -0,0 +1,20 @@
+package com.mzm.Fitpin.entity;
+
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+public class ItemSearchwithImg {
+    private int itemKey;
+    private String itemName;
+    private String itemType;
+    private String itemBrand;
+    private String itemStyle;
+    private int itemCnt;
+    private String itemContent;
+    private int itemPrice;
+    private Date itemDate;
+
+    private String itemImgURL; // 추가 리턴
+}
