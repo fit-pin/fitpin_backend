ca1db3843fbd004bf448413689e938589f166574
S-W-J <devilonprison@gmail.com>
2024-10-13 04:42:19 +0900

    결제내역 API 추가

---
+++ src/main/java/com/mzm/Fitpin/dto/order/PaymentDTO.java
@@ -0,0 +1,24 @@
+package com.mzm.Fitpin.dto.order;
+
+import lombok.Data;
+
+@Data
+public class PaymentDTO {
+    private int paymentId;
+    private int orderKey;
+    private int userKey;
+    private String userName;
+    private String userAddr;
+    private String userNumber;
+    private int itemKey;
+    private String itemName;
+    private String itemImg;
+    private String itemSize;
+    private int itemPrice;
+    private int itemTotal;
+    private int pitPrice;
+    private int pcs;
+    private String paymentMethod;
+    private int paymentAmount;
+
+}
