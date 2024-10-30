4c632a003c49751fbfdb50614485db4007a34843
S-W-J <devilonprison@gmail.com>
2024-10-31 03:48:58 +0900

    CORS 수정

--- src/main/java/com/mzm/Fitpin/config/WebConfig.java
+++ src/main/java/com/mzm/Fitpin/config/WebConfig.java
@@ -10,7 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8080", "http://fitpitback.kro.kr:8080")  // 로컬 및 배포 도메인 허용
+                .allowedOrigins(
+                        "http://localhost:8080",
+                        "http://fitpitback.kro.kr:8080",
+                        "http://localhost:3000",
+                        "https://fit-pin.github.io"
+                )  // 로컬 및 배포 도메인 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true); // 자격 증명 허용 (쿠키, Authorization 헤더)
